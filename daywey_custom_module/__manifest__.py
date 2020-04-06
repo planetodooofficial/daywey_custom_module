8fbd63a77fcda966b0575172fedb8a9a7e437583
https://Anjeel@bitbucket.org <https://Anjeel@bitbucket.org>
2020-04-06 13:56:09 +0530

    Commit

---
+++ daywey_custom_module/__manifest__.py
@@ -0,0 +1,19 @@
+{
+  "name"                 :  "Daywey Chen Custom",
+  "summary"              :  "Daywey Chen Custom",
+  "category"             :  "Tools",
+  "version"              :  "13.0.0",
+  "sequence"             :  1,
+  "author"               :  "Planet Odoo",
+  "company"              :  "Planet Odoo",
+  "website"              :  "http://www.planet-odoo.com/",
+  "description"          :  """Daywey Chen Custom""",
+  "depends"              :  [
+                             'base', 'purchase', 'product'
+                            ],
+  "data"                 :  [
+  ],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+}
